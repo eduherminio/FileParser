81a95b16023305506865fc1b9d3a67c5cba4d2b5
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-02-10 18:52:03 +0100

    [CI] Simplify travis-build.sh to test if it works this way

--- FileParserSolution/travis-build.sh
+++ FileParserSolution/travis-build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-#exit if any command fails
-set -e
-
 dotnet restore
-dotnet build --configuration Release --force --no-incremental
-
-revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
+dotnet build
