e62d35d15e7a1ec51ea6e2ee8bd2cc7e3d4553e7
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-02-10 18:47:52 +0100

    [CI] Separate build and tests

--- FileParserSolution/travis-test.sh
+++ FileParserSolution/travis-test.sh
@@ -3,8 +3,7 @@
 #exit if any command fails
 set -e
 
-dotnet restore
-dotnet build --configuration Release --force --no-incremental
+dotnet test ./FileParserTest/FileParserTest.csproj
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
