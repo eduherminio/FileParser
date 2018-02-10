194fe7646f9aba2b0d49748d516d3b2a43169816
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-02-10 19:00:00 +0100

    [CI] Try SO solution to build

--- FileParserSolution/travis-build.sh
+++ FileParserSolution/travis-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+#exit if any command fails
+set -e
+
 dotnet restore
-dotnet build
+dotnet build ./FileParser/ --configuration Release --force --no-incremental
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
