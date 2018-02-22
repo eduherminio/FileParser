0253b60d45109c4f8da51ea967a8e9a90d32e2c1
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-02-22 20:36:20 +0100

    Remove TODOs

--- FileParserSolution/FileParser/ParsedFile.cs
+++ FileParserSolution/FileParser/ParsedFile.cs
@@ -20,8 +20,6 @@ namespace FileParser
             _value = FileReader.ParseFile(path, existingSeparator, lineSeparatorToAdd);
         }
 
-        // TODO: DECIDE
-        // Is it worth keeping a null as return value, or throw exception as in ParsedLine (compulsory behavior in that case)
         public IParsedLine NextLine()
         {
             return _value.Count != 0
@@ -29,8 +27,6 @@ namespace FileParser
                 : throw new ParsingException("End of ParsedFile reached");
         }
 
-        // TODO: DECIDE
-        // Is it worth keeping a null as return value, or throw exception as in ParsedLine (compulsory behavior in that case)
         public IParsedLine PeekNextLine()
         {
             return _value.Count != 0
