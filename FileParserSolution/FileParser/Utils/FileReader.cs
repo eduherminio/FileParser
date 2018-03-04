eduherminio/FileParser

commit 8a1701a7cc6ab901f9b410ce99e996f2e64ce973

Minor style fixes (SonarLint)

FileParserSolution/FileParser/Utils/FileReader.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-//using Print = System.Console;
 using Print = System.Diagnostics.Debug;
 
 namespace FileParser
@@ -32,7 +31,7 @@ namespace FileParser
                     {
                         string original_line = reader.ReadLine();
                         // TODO: Evaluate if is it worth giving the user the option of detecting these kind of lines?
-                        if (string.IsNullOrWhiteSpace(original_line) || string.IsNullOrWhiteSpace(original_line))
+                        if (string.IsNullOrWhiteSpace(original_line) || string.IsNullOrEmpty(original_line))
                             continue;
                         // end TODO
                         Queue<string> parsedLine = new Queue<string>(ProcessLine(original_line, existingSeparator));
@@ -49,7 +48,7 @@ namespace FileParser
 
                 Print.WriteLine(e.Message);
                 Print.WriteLine("(path: {0}", path);
-                throw e;
+                throw;
             }
         }
 
@@ -81,7 +80,7 @@ namespace FileParser
 
                 Print.WriteLine(e.Message);
                 Print.WriteLine("(path: {0}", path);
-                throw e;
+                throw;
             }
         }
 
@@ -164,9 +163,7 @@ namespace FileParser
         /// <summary>
         /// Supported parsing conversions
         /// </summary>
-        static private HashSet<Type> SupportedTypes
-        {
-            get => new HashSet<Type>()
+        static private HashSet<Type> SupportedTypes { get; } = new HashSet<Type>()
             {
                 typeof(bool),
                 typeof(char),
@@ -177,6 +174,5 @@ namespace FileParser
                 typeof(double),
                 typeof(object)
             };
-        }
     }
 }

FileParserSolution/FileParser/Implementations/ParsedFile.cs
@@ -4,7 +4,7 @@ namespace FileParser
 {
     public class ParsedFile : IParsedFile
     {
-        private Queue<Queue<string>> _value;
+        private readonly Queue<Queue<string>> _value;
 
         public int Count { get => _value.Count; }
 

FileParserSolution/FileParserTest/ParsedFile.ExtractTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text;
 using System.IO;
 using Xunit;
 
@@ -11,7 +12,7 @@ namespace FileParserTest
     public class ExtractTests
     {
         [Fact]
-        void CustomLineParse()
+        public void CustomLineParse()
         {
             string fileName = "CustomLineParse.txt";
             string sampleContent = "  3  1154 508 100    vegetable ";
@@ -45,13 +46,13 @@ namespace FileParserTest
         }
 
         [Fact]
-        void ExtractChar()
+        public void ExtractChar()
         {
             string fileName = "ExtractChar.txt";
             string line1 = "+-*/!?#$%&";
             string line2 = "@()[]{}\"";
 
-            string parsedFile = string.Empty;
+            StringBuilder parsedFile = new StringBuilder(string.Empty);
 
             StreamWriter writer = new StreamWriter(fileName);
             using (writer)
@@ -68,14 +69,14 @@ namespace FileParserTest
             {
                 IParsedLine parsedLine = file.NextLine();
                 while (parsedLine.Count > 0)
-                    parsedFile += parsedLine.NextElement<char>();
+                    parsedFile.Append(parsedLine.NextElement<char>());
 
-                parsedFile += '\\';
+                parsedFile.Append('\\');
                 Assert.True(parsedLine.Empty);
             }
             Assert.True(file.Empty);
 
-            Assert.Equal(line1 + '\\' + line2 + '\\', parsedFile);
+            Assert.Equal(line1 + '\\' + line2 + '\\', parsedFile.ToString());
         }
     }
 }

FileParserSolution/FileParserTest/ParsedFile.ToListTests.cs
@@ -10,7 +10,7 @@ namespace FileParserTest
 {
     public class ToListTests
     {
-        private string _sampleFolderPath = "TestFiles" + System.IO.Path.DirectorySeparatorChar;
+        private readonly string _sampleFolderPath = "TestFiles" + System.IO.Path.DirectorySeparatorChar;
 
         [Fact]
         public void SameFileDifferentSeparators()

commit 526f96ee9c3fe5b3af671b6e62b03796936285fa

Fix bug introduced in 8a1701a7cc6ab901f9b410ce99e996f2e64ce973

FileParserSolution/FileParser/Utils/FileReader.cs
@@ -119,7 +119,7 @@ namespace FileParser
             }
             else if (typeof(T) == typeof(char))
             {
-                if (!wordsInLine.Any())
+                if (wordsInLine.Any())
                     throw new NotSupportedException("Extract<char> can only be used with one-length Queues" +
                         " Try using ExtractChar<string> instead, after parsing each string with Extract<string>(Queue<string>)");
 