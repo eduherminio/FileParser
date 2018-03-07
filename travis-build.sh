126e85d9dc69926fd15b22814765876d2fe24a64
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-03-07 21:56:19 +0100

    :construction_worker: Do fix Travis CI build

--- travis-build.sh
+++ travis-build.sh
@@ -4,7 +4,7 @@
 set -e 
 
 dotnet restore ./FileParserSolution 
-dotnet build ./FileParserSolution --configuration Release --force --no-incremental --framework=netstandard2.0
+dotnet build ./FileParserSolution/FileParser --configuration Release --force --no-incremental --framework=netstandard2.0
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
