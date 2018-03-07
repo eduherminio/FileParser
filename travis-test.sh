e3ab2693cc032cf21ec0067be651155be5af718b
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-03-07 21:49:31 +0100

    :construction_worker: Fix Travis CI build and tests
    Since Travis CI (sadly) doesn't support .NET Framework:
    * Build only targetting to netstandard2.0 framework
    * Test only targetting to netcoreapp2.0 framework

--- travis-test.sh
+++ travis-test.sh
@@ -3,7 +3,7 @@
 #exit if any command fails 
 set -e 
 
-dotnet test ./FileParserSolution/FileParserTest
+dotnet test ./FileParserSolution/FileParserTest --framework=netcoreapp2.0
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
