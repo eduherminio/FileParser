5498a567e29ddc5f678995750c32931ad45ab97f
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2019-05-04 12:03:27 +0200

    :construction_worker: Add CircleCI support (#12)
    * Rename FileParserSolution/ to src/.
    * Place build scripts in ci/
    * Add CircleCI support
    * Add CircleCI build status badge, try placing bagdes in a table.

---
+++ ci/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+dotnet restore ../src
+dotnet build ../src/FileParser --configuration Release --force --no-incremental --framework netstandard2.0
+
+revision=${TRAVIS_JOB_ID:=1}
+revision=$(printf "%04d" $revision)
--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@
 #exit if any command fails
 set -e
 
-dotnet test ./FileParserSolution/FileParser.Test --framework=netcoreapp2.2
+dotnet test ../src/FileParser.Test --framework=netcoreapp2.2
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
