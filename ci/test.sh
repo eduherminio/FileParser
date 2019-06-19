d4be0ba2f8471baa05a22d24c8a9236487a70aa9
Eduardo Caceres <edu_kueyar@hotmail.com>
2019-06-20 00:26:11 +0100

    Update azure-pipelines.yml following specific .NET Core instructions https://docs.microsoft.com/es-es/azure/devops/pipelines/languages/dotnet-core?view=azure-devops

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@
 #exit if any command fails
 set -e
 
-dotnet test ../src/FileParser.Test --framework=netcoreapp2.2
+dotnet test ../src/FileParser.Test --framework=netcoreapp2.2 --logger trx --collect "Code coverage"
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
