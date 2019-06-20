63a880dc3f302cef0919c770c03ea963978485b7
Eduardo Caceres <edu_kueyar@hotmail.com>
2019-06-20 01:10:59 +0100

    Testing build.sh modification to asume it's being invoked from outside ci folder

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,8 @@
 #exit if any command fails
 set -e
 
-dotnet restore ../src
-dotnet build ../src/FileParser --configuration Release --force --no-incremental --framework netstandard2.0
+dotnet restore ./src
+dotnet build ./src/FileParser --configuration Release --force --no-incremental --framework netstandard2.0
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
